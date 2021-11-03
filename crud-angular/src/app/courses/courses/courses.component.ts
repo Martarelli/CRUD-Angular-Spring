611931b5ca23df9b095f7b4563b81531cb4ef4f4
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2021-11-03 20:50:44 -0300

    adicionado service

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,8 @@
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
-import { Course } from '../model/course'
+
+import { Course } from '../model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
@@ -7,14 +10,14 @@ import { Course } from '../model/course'
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    {_id: '1', name: 'Angular', category:'front-end'}
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name','category']
 
-
-  constructor() {
+  //coursesService: CoursesService
+  constructor(private coursesService: CoursesService) {
     // this.courses = [];
+    //this.coursesService = new CoursesService;
+    this.courses = this.coursesService.list();
    }
 
   ngOnInit(): void {
